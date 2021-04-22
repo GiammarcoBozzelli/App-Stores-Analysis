d29fae1ae60c381388ba467b11c1ab8aa8ccd645
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2021-04-22 19:50:03 +0200

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -6,9 +6,9 @@ import os.path
 
 #%%
 #Get file 'playstore.csv' and remove all columns not useful in answering the first research question
-# 
-print(os.getcwd())
-frame = pd.read_csv(os.path.join( os.getcwd(), 'playstore.csv'))
+
+frame = pd.read_csv(os.path.join( os.getcwd(), 'playstore.csv')) #open as DataFrame the file
+print(frame.columns) #checking all columns' labels
 frame = frame.drop(columns=['id'])
 frame.drop(frame.loc[:,'rate_5_pc':'updated'], inplace = True, axis = 1)
 frame.drop(frame.loc[:,'current_version':'in_app_products'], inplace = True, axis = 1)
