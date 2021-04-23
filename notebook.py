d06fc2f5f7d9a934d45c2c633afb1f9bf879bb7e
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2021-04-23 12:33:28 +0200

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -10,10 +10,14 @@ import os.path
 frame = pd.read_csv(os.path.join( os.getcwd(), 'playstore.csv')) #open as DataFrame the file
 frame.columns #checking all columns' labels
 playstore = frame.loc[:,['name','category','rating','reviews','price','size','installs']]
-#playstore.info
+playstore.info
 #playstore
-playstore.isna().sum() #number of cells with NAN values per each column
-
+#%%
+#Removing all rows with null values
+print('\n Number of NaN values in each column \n', playstore.isna().sum()) #number of cells with NAN values per each column
+playstore.dropna(axis=0, how='any', inplace=True) #deleting all rows with at least one NaN
+playstore.isna().sum() #checking if there are Nan left
+playstore.info()
 #%%
 #Group all data per category and visualize how many apps are in each category 
 
