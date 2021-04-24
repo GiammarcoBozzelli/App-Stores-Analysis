11fd388bf460d65dd21d915f86b3f1e2d137b0a1
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2021-04-24 16:04:51 +0200

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -68,16 +68,47 @@ windowstore
 #%%
 #Calculating the actual rating for each category
 categories = set(windowstore['category'].tolist()) #getting all category types
-category_rating_dic = {} #dict to contain the rating for each category type
+category_rating_dic2 = {} #dict to contain the rating for each category type
 for category in categories: #filling the dictionary
     cat = windowstore.loc[windowstore['category'] == category] #choosing the single category
     rating = cat['weighted_rating'].sum() / cat['reviews'].sum() #calculating the rating wrt n. of installs
-    category_rating_dic[category] = rating #adding the new key:value to the dictionary
-print(category_rating_dic)
+    category_rating_dic2[category] = rating #adding the new key:value to the dictionary
+print(category_rating_dic2)
 #%%
 
 # making a bar char with the top 5 categories
 import matplotlib.pyplot as plt
-d = sorted(category_rating_dic.items(), key=lambda xy:xy[1], reverse = True)[:6]
+d = sorted(category_rating_dic2.items(), key=lambda xy:xy[1], reverse = True)[:6]
+plt.bar([x[0] for x in d], [x[1] for x in d],log = True) # use log = True to make the graph more evident
+plt.show()
+
+#%%
+#%%
+#APPSTORE
+frame3 = pd.read_csv(os.path.join(os.getcwd(), 'AppleStore.csv')) #open as DataFrame the file
+print(frame3.columns) #checking all columns' labels
+appstore = frame3.loc[:, ['name', 'category', 'user_rating', 'rating_count_tot' ]]
+appstore.info()
+#%%
+appstore.loc[:,'weighted_rating'] = appstore['user_rating'] * appstore['rating_count_tot']
+appstore
+
+
+#%%
+
+#Calculating the actual rating for each category
+categories = set(appstore['category'].tolist()) #getting all category types
+category_rating_dic3 = {} #dict to contain the rating for each category type
+for category in categories: #filling the dictionary
+    cat = appstore.loc[appstore['category'] == category] #choosing the single category
+    rating = cat['weighted_rating'].sum() / cat['rating_count_tot'].sum() #calculating the rating wrt n. of installs
+    category_rating_dic3[category] = rating #adding the new key:value to the dictionary
+print(category_rating_dic3)
+
+#%%
+
+# making a bar char with the top 5 categories
+import matplotlib.pyplot as plt
+d = sorted(category_rating_dic3.items(), key=lambda xy:xy[1], reverse = True)[:6]
 plt.bar([x[0] for x in d], [x[1] for x in d],log = True) # use log = True to make the graph more evident
 plt.show()
