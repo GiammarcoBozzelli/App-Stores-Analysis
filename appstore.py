8b8f5d79a6282bf4080e549252ebc41cc9fb0e59
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2021-04-25 17:35:46 +0200

    Update appstore.py

--- appstore.py
+++ appstore.py
@@ -37,5 +37,5 @@ def bar_chart (category_rating_dic):
     # making a bar char with the top 5 categories
     d = sorted(category_rating_dic.items(), key=lambda xy: xy[1], reverse=True)[:5]
     print('The top 5 categories are:', d)
-    plt.bar([x[0] for x in d], [x[1] for x in d], log=True)# use log = True to make the graph more evident
+    plt.bar([x[0] for x in d], [x[1] for x in d], log=True)# use log = True to make the graph clearer
     plt.show()
