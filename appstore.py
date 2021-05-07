f19fc818deb7b93ba07dc421e637a36e7a10949a
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2021-05-07 18:01:05 +0200

    Update appstore.py

--- appstore.py
+++ appstore.py
@@ -7,14 +7,16 @@ import matplotlib.pyplot as plt
 
 def top_categories_weighted(data,store_type = None):
     frame = data.copy()
-    frame['weighted_rating'] = frame['interactions'] * frame['rating'] # multiply the ratign * number of interaction and divide by the total number of interactions
+    frame['interactions'].dropna(inplace = True)#remove all rows in 'interaction' and 'rating' column with NaN values
+    frame['rating'].dropna(inplace = True)
+    frame['weighted_rating'] = frame['interactions'] * frame['rating'] # multiply the rating * number of interaction and divide by the total number of interactions
 
     if store_type is None: #check whether the store type was passed as a variable or not
         pass
     else:
         frame = frame[frame['store'] == store_type] #select only the rows with the store type that was called
 
-    frame = frame[frame['category'].notnull()] #get only the rows with non- NaN values in the 'category' column
+    frame = frame[frame['category'].notnull()] #get only the rows with non- NaN values
     categories = set(frame['category'].tolist())  # getting all category types
     category_rating_dic = {} # dict to contain the rating for each category type
     for category in categories:  # filling the dictionary
@@ -25,4 +27,12 @@ def top_categories_weighted(data,store_type = None):
 
 '''def bar_chart(category_rating_dic):
     # making a bar char with the top 5 categories
+'''
+
+'''import re
+
+def clean_column (df, column):
+    df[column] = df[column].str.replace(r'\D', '')
+    df[column].isna = 0.0
+    return df[column]
 '''
