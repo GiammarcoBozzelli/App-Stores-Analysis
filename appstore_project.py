dccd37e606d74ffa2b4fd6b76f5a4aa0779ed192
Giammarco Bozzelli <giammarcobozzelli3006@gmail.com>
2021-05-12 21:02:45 +0200

    Update appstore_project.py

--- appstore_project.py
+++ appstore_project.py
@@ -6,44 +6,42 @@ import numpy as np
 import pandas as pd
 import os.path
 import appstore as app
-import seaborn as sb
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 #%%
 
 #DATA LOADING - CLEANING
-data_folder = os.getcwd()
-file_dir_names = os.listdir(data_folder)
+data_folder = os.getcwd() #get the current working directory that we will use as path
+file_dir_names = os.listdir(data_folder) #list of all files in the directory
 print(data_folder,'\n', file_dir_names)
 
 #%%
 
 #PLAYSTORE
 #Importare il dataframe
-playstore = pd.read_csv(os.path.join(data_folder,'playstore.csv'))
+playstore = pd.read_csv(os.path.join(data_folder,'playstore.csv'))# open playstore.csv
 
 #%%
 
 #pulire le colonne
-playstore = playstore.loc[:,["app_name","genre","rating","reviews","cost_label","size","installs"]]
+playstore = playstore.loc[:,["app_name","genre","rating","reviews","cost_label","size","installs"]] #get only the columns we need
 
-playstore.columns = ["name","category","rating","reviews","price","size (MB)","installs"]
-playstore
+playstore.columns = ["name","category","rating","reviews","price","size (MB)","installs"] #renaming all columns
 
 #%%
 
-#pulire la colonna del prezzo
-import string
-playstore['price'] = playstore['price'].astype(str)
+#clean the 'price' column
+playstore['price'] = playstore['price'].astype(str) #convert all cells in the columns into strings
 
-bad = [",","₫"," Buy"]
+bad = [",","₫"," Buy"] #list of all things to remove
 for s in bad:
     playstore["price"] = playstore["price"].str.replace(s,"")
 
 playstore['price'] = playstore['price'].str.replace('Install', '0')
-playstore['price'] = playstore['price'].str.split(' ').str[-1]
+playstore['price'] = playstore['price'].str.split(' ').str[-1] #was 200 now is 7
 
-#convertire la colonna del prezzo in numero decimale e convertire in USD
+#convert the 'price' column to float32 and from vietnamese dong to USD
 playstore['price'] = np.float32(playstore['price'])*0.000043
 
 #%%
@@ -60,16 +58,11 @@ playstore['installs'] = playstore['installs'].str.strip('+')
 playstore['installs'] = playstore['installs'].str.replace(',', '')
 playstore['reviews'] = playstore['reviews'].str.replace(' total', '')
 playstore['reviews'] = playstore['reviews'].str.replace(',', '')
-playstore
 
 #%%
 
 playstore['size (MB)'] = np.float64(playstore['size (MB)'])
-
-#%%
-
-#aggiungere una nuova colonna che specifica lo store
-playstore["store"] = "playstore"
+playstore["store"] = "playstore" #add a column to specify the store
 
 #%%
 
@@ -79,31 +72,31 @@ playstore
 #%%
 
 #WINDOWS
-#Importare il dataframe
+#Import the dataset
 windows_store = pd.read_csv(os.path.join(data_folder,'msft.csv'))
 
 #%%
 
-#pulire le colonne
-windows_store = windows_store.loc[:,["Name","Rating","No of people Rated","Category","Price"]]
-windows_store.columns = ["name","rating","No of people Rated","category","price"]
-windows_store['size (MB)'] = 0.0
+#clean columns
+windows_store = windows_store.loc[:,["Name","Rating","No of people Rated","Category","Price"]] #get only the columns we nee
+windows_store.columns = ["name","rating","No of people Rated","category","price"]#renaming the columns
+windows_store['size (MB)'] = 0.0 #add 0 because we do not want nan in this colums when we merge all datasets
 
 #%%
 
-#pulire la colonna del prezzo
+#clean the 'price' column
 windows_store['price'] = windows_store['price'].str.replace('Free', '0')
 windows_store['price'] = windows_store['price'].str.replace('₹ ', '')
 
 windows_store['price'] = windows_store['price'].astype(str)
-windows_store['price'] = windows_store['price'].str.strip('+, ')
+windows_store['price'] = windows_store['price'].str.strip('+, ')#remove leading and trailing single characters
 
-#convertire la colonna del prezzo in numero decimale e convertire in USD
+#convert the 'price' column to float64 and from Indian rupee to USD
 windows_store['price'] = np.float64(windows_store['price'].str.replace(',', ''))*0.013
 
 #%%
 
-#aggiungere una nuova colonna che specifica lo store
+#add the 'store' column
 windows_store["store"] = "windows_store"
 
 #%%
@@ -114,16 +107,16 @@ windows_store
 #%%
 
 #APPLESTORE
-#Importare il dataframe
+#import the dataset
 appstore = pd.read_csv(os.path.join(data_folder,'AppleStore.csv'))
 appstore
 
 #%%
 
-#pulire le colonne
+#clean columns
 appstore = appstore.loc[:,["name","size_bytes","price","installs","rating","category", "rating_count_ver"]]
 appstore.columns = ["name","size (MB)","price","installs","rating","category","No of people Rated"]
-appstore['size (MB)'] = np.float64(appstore['size (MB)'])/1000000
+appstore['size (MB)'] = np.float64(appstore['size (MB)'])/1000000 #convert from bytes to megabytes
 
 #%%
 
@@ -134,46 +127,48 @@ appstore
 
 #%%
 
-#UNISCO I DATAFRAME
-alldata = pd.DataFrame()
+#Merge Dataframes
 alldata = pd.concat([playstore,appstore,windows_store])
 alldata
 
 #%%
 
-#UNISCO LE COLONNE
+#Clean columns
 alldata['No of people Rated'] = alldata['No of people Rated'].fillna(0)
 alldata['reviews'] = alldata['reviews'].fillna(0)
 
-alldata['interactions'] = alldata['reviews'] + alldata['No of people Rated']
-alldata = alldata.loc[:,['name','price','interactions','category', 'store', 'rating', 'size (MB)']]
+alldata['interactions'] = alldata['reviews'] + alldata['No of people Rated'] #merge 'reviews' and 'No of people rated' into 'interaction'
+alldata = alldata.loc[:,['name','price','interactions','category', 'store', 'rating', 'size (MB)']] #select only comuns we want
 
-alldata = alldata.dropna(thresh=4)
+alldata = alldata.dropna(thresh=4) #remove rows with more than 4 NaN values
 
 #%%
 
-dict_categories = {'Health & Fitness':['Beauty','Health & Fitness','Medical','Health and Fitness'],
+dict_categories = {'Health & Fitness':['Beauty','Medical','Health and Fitness'],
                        'Photos & Videos':['Video Players & Editors','Art & Design','Photography', 'Multimedia Design', 'Photo & Video'],
-                       'News':['Weather', 'News & Magazines', 'Sports','News and Weather', 'Weather', 'News'],
+                       'News':['Weather', 'News & Magazines', 'Sports','News and Weather', 'Weather'],
                        'Games':['Puzzle', 'Adventure', 'Action', 'Arcade', 'Trivia', 'Word', 'Music', 'Casual', 'Card', 'Simulation', 'Role Playing', 'Strategy', 'Casino', 'Comics', 'Board', 'Educational', 'Racing'],
-                       'Music':['Music & Audio', 'Music'],
-                       'Business': ['Finance', 'Government and Politics', 'Business'],
-                       'Social' : ['Communication', 'Dating', 'Entertainment', 'Social', 'Events', 'Social Networking'],
-                       'Lifestyle' : ['Personalization', 'Shopping', 'Lifestyle'],
+                       'Music':['Music & Audio'],
+                       'Business': ['Finance', 'Government and Politics'],
+                       'Social' : ['Communication', 'Dating', 'Entertainment', 'Events', 'Social Networking'],
+                       'Lifestyle' : ['Personalization', 'Shopping'],
                        'Tools & Utilities' : ['Productivity', 'Tools', 'Libraries & Demo','House & Home', 'Catalogs', 'Utilities','Developer Tools'],
-                       'Food & Drink' : ['Food & Drink', 'Food and Dining'],
-                       'Maps & Navigation' : ['Auto & Vehicles', 'Maps & Navigation', 'Travel', 'Travel & Local', 'Navigation and Maps', 'Navigation'],
-                       'Books & Reference' : ['Books & Reference', 'Books', 'Book', 'Reference'],
-                       'Education' : ['Education', 'Parenting', 'Kids and Family']}
-
-#QUESTION N. 1
+                       'Food & Drink' : ['Food and Dining'],
+                       'Maps & Navigation' : ['Auto & Vehicles', 'Travel', 'Travel & Local', 'Navigation and Maps', 'Navigation'],
+                       'Books & Reference' : ['Books', 'Book', 'Reference'],
+                       'Education' : ['Parenting', 'Kids and Family']}
 
 
 for k,v in dict_categories.items():
     for x in v:
-        alldata['category'].replace(x, k, inplace = True)
+        alldata['category'].replace(x, k, inplace = True) #replace sub categories with the categories we want
 
 alldata
+pie = app.pie_chart(alldata)
+
+#%% md
+
+<h2> Question N.1 <h2>
 
 #%%
 
@@ -230,7 +225,7 @@ app.grafic_rating(wscateg_dict)
 #%% md
 
 <h2>Research Question 2</h2>
-<p>For which App Category are users willing to pay more?<br>
+<p><Which category are the most expensive for the users?<br>
 
 1) For each app with same category we compute the price mean<p>
 2) Then we represent it graphically <p>
@@ -241,7 +236,7 @@ app.grafic_rating(wscateg_dict)
 df = alldata.copy()
 
 #Selecting the essential columns for our research question
-df = df[['category', 'price']]
+df = df[['category', 'price','store']]
 df
 
 #%% md
@@ -273,7 +268,6 @@ We can now work on the research question:<p>
 #%%
 
 #Finding all the categories and storing them in a list
-price_mean_dict= {}
 
 categories = (df['category'].tolist())  # getting all category types
 category_list = [] # dict to contain all the categories
@@ -288,16 +282,17 @@ category_list
 #%%
 
 #Use the previous list to create a dictionary with the price mean of each category
+price_mean_dict= {}
 for x in category_list:
     a = df[df['category']==x]
-    price_mean_dict[x]=a['price'].mean(axis=0)
+    price_mean_dict[x]=a['price'].mean()
 
 price_mean_dict
 
 
 #%%
 
-#Visualising the dictionary in a series
+#Visualising the dictionary in a dataframe
 
 df_mean_price = pd.Series(price_mean_dict).rename_axis('category').reset_index(name='mean price')
 df_mean_price
@@ -305,7 +300,7 @@ df_mean_price
 #%%
 
 #sorting values in ascending order
-df_mean_price.sort_values(by=['mean price'], inplace=True)
+df_mean_price.sort_values(by=['mean price'], inplace=True, ascending = False)
 df_mean_price
 
 #%% md
@@ -323,21 +318,74 @@ As we can clearly see from the data, the categories: Education and Tool & Utilit
 
 df_mean_price.plot.barh(x='category', y='mean price', rot=0, color=['orange','blue'])
 
-#%%
-
-#Computing summary statistic
-df_mean_price.describe()
-
 #%% md
 
 <h4> Conclusion <h4>
 
 #%% md
 
-The catgories below categories are the most expensive among all the stores
+The categories below are the most expensive among all stores
 - Education
 - tools & Utilities
 - Health & Fitness
+
+#%%
+
+x=alldata["size (MB)"]
+y=alldata['price']
+
+#%%
+
+def linear_regression(x, y):
+    N = len(x)
+    x_mean = x.mean()
+    y_mean = y.mean()
+
+    B1_num = ((x - x_mean) * (y - y_mean)).sum()
+    B1_den = ((x - x_mean)**2).sum()
+    B1 = B1_num / B1_den
+
+    B0 = y_mean - (B1*x_mean)
+
+    reg_line = 'y = {} + {}β'.format(B0, round(B1, 3))
+
+    return (B0, B1, reg_line)
+
+#%%
+
+def corr_coef(x, y):
+    N = len(x)
+
+    num = (N * (x*y).sum()) - (x.sum() * y.sum())
+    den = np.sqrt((N * (x**2).sum() - x.sum()**2) * (N * (y**2).sum() - y.sum()**2))
+    R = num / den
+    return R
+
+#%%
+
+B0, B1, reg_line = linear_regression(x, y)
+print('Regression Line: ', reg_line)
+R = corr_coef(x, y)
+print('Correlation Coef.: ', R)
+print('"Goodness of Fit": ', R**2)
+
+#%%
+
+# Import matplotlib and seaborn libraries to visualize the data
+
+data = alldata.drop(alldata[(alldata['price'] >= 60)].index)
+#alldata=sns.load_dataset(alldata)
+g = sns.regplot(x="size (MB)", y="price",data=alldata, color="g")
+g= sns.jointplot(x="size (MB)", y="price",data=alldata, kind="reg")
+regline = g.ax_joint.get_lines()[0]
+regline.set_color("red")
+
+#%%
+
+# Using pairplot we'll visualize the data for correlation
+sns.pairplot(data, x_vars="size (MB)",
+             y_vars="price", size=4, aspect=1, kind='scatter')
+plt.show()
 
 #%%
 
@@ -473,3 +521,6 @@ top_50 = top_50.loc[top_50['price'] == 0]
 top_50 = top_50.loc[top_50['store'] == 'appstore']
 
 top_50
+
+#%%
+
